aca20f77aa89b65a594851afa4164da41dd27ed3
Serji2158 <sergey.tyschenko@gmail.com>
2021-08-19 23:34:53 +0300

    Update task-03.js

--- goit-js-hw-07/src/js/task-03.js
+++ goit-js-hw-07/src/js/task-03.js
@@ -1,3 +1,15 @@
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li. 
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +24,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('#gallery');
+
+const galleryList = images =>{ 
+return images.map(image => `<li><img src= "${image.url}" alt= "${image.alt}"></li>`);
+ );
+
+ console.log(galleryList);
+  gallery.insertAdjacentHTML('beforeend', galleryList(images).join(''))
+  // console.log(gallery);
+
+
+
+
+
+
